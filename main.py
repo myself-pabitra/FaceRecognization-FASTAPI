847ca9ce673523bf360a1f2c25a418f7a2e8d531
pabitra <pabitrapandit2001@gmail.com>
2023-10-13 13:41:07 +0530

    Updated readme and image checking

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 import face_recognition
+from typing import List
 
 
 app = FastAPI()
@@ -9,22 +10,37 @@ class Result(BaseModel):
     match: bool
     error: str = None
 
+
 @app.post('/images', response_model=Result)
 async def post_images(file1: UploadFile = File(...), file2: UploadFile = File(...)):
     try:
         first_image = face_recognition.load_image_file(file1.file)
         second_image = face_recognition.load_image_file(file2.file)
 
-        first_face_encoding = face_recognition.face_encodings(first_image)[0]
-        second_face_encoding = face_recognition.face_encodings(second_image)[0]
+        first_face_encodings = face_recognition.face_encodings(first_image)
+        second_face_encodings = face_recognition.face_encodings(second_image)
 
-        result = face_recognition.compare_faces([first_face_encoding], second_face_encoding)
+        first_face_detected = len(first_face_encodings) > 0
+        second_face_detected = len(second_face_encodings) > 0
 
-        return Result(match=result[0])
+        if not first_face_detected and not second_face_detected:
+            return Result(match=False, error="Error: No face detected in both images")
+        elif not first_face_detected:
+            return Result(match=False, error="Error: No face detected in file1")
+        elif not second_face_detected:
+            return Result(match=False, error="Error: No face detected in file2")
+        else:
+            first_face_encoding = first_face_encodings[0]
+            second_face_encoding = second_face_encodings[0]
+
+            result = face_recognition.compare_faces([first_face_encoding], second_face_encoding)
+
+            return Result(match=result[0])
 
     except FileNotFoundError:
         return Result(match=False, error="Error: File not found")
 
-    except IndexError:
-        return Result(match=False, error="Error: No face found in one or both images")
+    except Exception as e:
+        return Result(match=False, error=f"Error: {str(e)}")
+
 
